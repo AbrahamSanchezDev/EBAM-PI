25019473fb9ffb20c5398d6c40af420890117bd2
Jennifer <612210329@utsalamanca.edu.mx>
2025-07-06 15:11:27 -0600

    Acomodo de logo

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -6,13 +6,19 @@ import { signOut } from "@/auth";
 
 export default function SideNav() {
   return (
-    <div className="flex h-full flex-col px-3 py-4 md:px-2">
+    <div className="flex h-full  flex-col px-3 py-4 md:px-2">
       <Link
-        className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-600 p-4 md:h-40"
+        className="mb-2 flex h-25 items-end justify-center rounded-md bg-black p-4 md:h-50"
         href="/"
       >
-        <div className="w-32 text-white md:w-40">
-          <AcmeLogo />
+        {/* Responsive: logo a la izquierda del nombre en móvil, en columna en md+ */}
+        <div className="w-40 text-white md:w-50 flex flex-row md:flex-col justify-center items-center">
+          <img
+            src="/logo EBAM.png"
+            alt="Logo EBAM"
+            className="h-12 w-12 md:h-20 md:w-20 mr-2 md:mb-2 md:mr-0"
+          />
+          <p className="text-[28px] md:text-[44px]">EBAM</p>
         </div>
       </Link>
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
