0dc641e496f4314a66c7dc95095950497a81529c
Jennifer <612210329@utsalamanca.edu.mx>
2025-07-06 14:32:04 -0600

    Perfiles actualizado

--- app/dashboard/perfiles/page.tsx
+++ app/dashboard/perfiles/page.tsx
@@ -1,19 +1,16 @@
+"use client";
 import { lusitana } from "@/app/ui/fonts";
-import { Metadata } from "next";
 import RFIDReader from "@/app/esp32/RFIDReader";
+import { InfoUsuario } from "@/app/ui/perfiles/infoUsuario";
 
-export const metadata: Metadata = {
-  title: "Perfiles",
-};
-
-export default async function Page(props: {}) {
+export default function Page(props: {}) {
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
         <h1 className={`${lusitana.className} text-2xl`}>Perfiles</h1>
       </div>
-
-      <RFIDReader />
+      <InfoUsuario />
+      {/* <RFIDReader /> */}
     </div>
   );
 }
