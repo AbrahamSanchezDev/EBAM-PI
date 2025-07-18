0c216488cd721a2c78ef128f48d7e63102eb6ac1
Fernanda-Torres5 <marcos-11954@hotmail.com>
2025-07-17 18:32:08 -0600

    Pagina de control de calendarios

---
+++ app/dashboard/control-calendario/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { MyCalendar } from "../(overview)/MyCalendar";
+
+export default function Page() {
+  return (
+    <main>
+      <h1 className="mb-4 text-xl md:text-2xl">Control de calendario</h1>
+      <MyCalendar />
+    </main>
+  );
+}
