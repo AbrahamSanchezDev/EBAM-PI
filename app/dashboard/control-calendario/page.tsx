b832e1b5b53be2704245a798f160407e64317cff
Fernanda-Torres5 <marcos-11954@hotmail.com>
2025-07-17 18:50:44 -0600

    Se puede guardar, editar y eliminar

--- app/dashboard/control-calendario/page.tsx
+++ app/dashboard/control-calendario/page.tsx
@@ -1,11 +1,12 @@
 "use client";
-import { MyCalendar } from "../(overview)/MyCalendar";
+import CrudCalendar from "@/app/componentes/CrudCalendar";
+import { MyCalendar } from "../../componentes/MyCalendar";
 
 export default function Page() {
   return (
     <main>
       <h1 className="mb-4 text-xl md:text-2xl">Control de calendario</h1>
-      <MyCalendar />
+      <CrudCalendar />
     </main>
   );
 }
