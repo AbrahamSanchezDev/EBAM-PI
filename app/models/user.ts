65ef265a21171c3dfa794aa7efcc877e27ff35de
Jennifer <612210329@utsalamanca.edu.mx>
2025-07-27 11:17:10 -0600

    crear usuarios de prueba en profiles

--- app/models/user.ts
+++ app/models/user.ts
@@ -1,15 +1,35 @@
-export interface User {
+export interface Profile {
   name: string;
   email: string;
   password: string;
   role: "admin" | "user";
+  matricula: string;
+  carrera: string;
+  grupo: string;
+  rfids: { id: string; active: boolean }[];
+  calendarIds: string[];
 }
 
-export const createUser = (
+export const createProfile = (
   name: string,
   email: string,
   password: string,
-  role: "admin" | "user"
-): User => {
-  return { name, email, password, role };
+  role: "admin" | "user",
+  matricula: string,
+  carrera: string,
+  grupo: string,
+  rfids: { id: string; active: boolean }[],
+  calendarIds: string[]
+): Profile => {
+  return {
+    name,
+    email,
+    password,
+    role,
+    matricula,
+    carrera,
+    grupo,
+    rfids,
+    calendarIds,
+  };
 };
